8774e79170002e5461bcea83a0cd01273bff9ac0
Variot <variot.anthony@gmail.com>
2018-06-10 23:12:40 +0200

    Création d'un article
    Ajout de la page pour créer un article

--- routes/posts.js
+++ routes/posts.js
@@ -1,13 +1,31 @@
 module.exports = function(app) {
-  app.get("/post/create", function(req, res) {
-    res.send("XXX");
+
+  /**
+  * 	On récupère ici les donées du formulaire pour insérer un nouvel article en base
+  * */
+  app.post("/nouvelarticle", function(req, res) {
+    var article = req.body;
+
+    app.db.collection('articles').find({}).toArray(function(err, articles) {
+      if(err) {
+        console.log("Erreur: ", err);
+      }
+
+
+      app.db.collection('articles').insertOne({
+        titre: article.titre, 
+        auteur: article.auteur,  
+        resume: article.resume, 
+      });
+    });
+
+    res.redirect("http://localhost:8000/creation");
   });
 
-  app.post("/post/create", function(req, res) {
-    res.send("XXX");
-  });
-
-  app.get("/post/:id", function(req, res) {
-    res.send("XXX");
+   /**
+   * 	On affiche à l'utilisateur le formulaire de création
+   */
+  app.get("/post/creation", function(req, res) {
+    res.render("insertion");
   });
 }
